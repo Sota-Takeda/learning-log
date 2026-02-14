20f8ecdf34ba1885552d4e24343e1d184407cb74
Sota-Takeda <220647849+Sota-Takeda@users.noreply.github.com>
2026-02-14 20:33:02 +0900

    fix: clear config cache before migrate

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,19 +1,18 @@
+cat > scripts/00-laravel-deploy.sh <<'EOF'
 #!/usr/bin/env bash
 set -e
 
-echo "Running composer"
+echo "Running composer..."
 composer install --no-dev --working-dir=/var/www/html --optimize-autoloader --no-interaction
 
 echo "Clearing caches..."
 php artisan config:clear || true
-php artisan route:clear || true
-php artisan view:clear || true
+php artisan route:clear  || true
+php artisan view:clear   || true
 
 echo "Caching config..."
 php artisan config:cache
 
-echo "Caching routes..."
-php artisan route:cache || true
-
 echo "Running migrations..."
 php artisan migrate --force -n
+EOF
