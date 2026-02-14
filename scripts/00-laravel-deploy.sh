b14b6d9ab7adac7023a8325f355a59447a785611
Sota-Takeda <220647849+Sota-Takeda@users.noreply.github.com>
2026-02-14 20:04:10 +0900

    chore: recache config after env changes

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html --optimize-autoloader --no-interaction
+
+echo "Clearing caches..."
+php artisan config:clear || true
+php artisan route:clear || true
+php artisan view:clear || true
 
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
-php artisan route:cache
+php artisan route:cache || true
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force -n
